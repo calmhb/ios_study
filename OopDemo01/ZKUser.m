0a9531f5244f91e80b739d0a89bb09e76a8d4aef
Mac <mac@ayolwas-3.local>
2016-03-02 16:49:35 +0800

    模拟类变量

---
+++ OopDemo01/ZKUser.m
@@ -0,0 +1,35 @@
+//
+//  ZKUser.m
+//  OopDemo01
+//
+//  Created by Mac on 16/3/2.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+//ZKUser的定义部分
+@interface ZKUser : NSObject
++(NSString*) nation;
++(void) setNation:(NSString*) newNation;
+@end
+
+static NSString* nation=nil;
+@implementation ZKUser
++(NSString*) nation{
+    return nation;//返回nation全局变量
+}
++(void)setNation:(NSString*)newNation{
+   //对nation全局变量赋值
+    if(![nation isEqualToString:newNation]){
+        nation=newNation;
+    }
+}
+@end
+
+int main(int argc,char* argv[]){
+    @autoreleasepool {
+        [ZKUser setNation:@"中国"];//为ZKUser的类变量赋值
+        NSLog(@"nation的值为：%@",[ZKUser nation]);//中国
+    }
+}
+
