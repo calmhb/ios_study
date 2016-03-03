888b6c407a27d08127a904c9183d633ed9614b50
Mac <mac@ayolwas-3.local>
2016-03-03 09:05:49 +0800

    @private访问修饰符(2)

--- OopDemo01/ZKTeacherTest.m
+++ OopDemo01/ZKTeacherTest.m
@@ -13,8 +13,15 @@ int main(int argc,char* argv[]){
     @autoreleasepool {
         //实例化ZKTeacher对象
         ZKTeacher* t1=[[ZKTeacher alloc] init];
-        
-        
+        //age成员变量已经隐藏，下面语句编译报错
+        // t1->_age=1000;
+        //通过setAge方法对age赋值，但是不符合规则
+        [t1 setAge:1000];//年龄不合法
+        //获得age的值
+        NSLog(@"设置age成员变量:%d",[t1 age]);//设置age成员变量：0
+        //成功修改t1的age变量
+        [t1 setAge:30];
+        NSLog(@"设置age成员变量:%d",[t1 age]);//设置age成员变量：30
     }
 }
 
